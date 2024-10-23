f0b6e3511d1be5ccc16d51b59182e92618980c55
Martossy Alex <github@mail.martossy.hu>
2024-10-23 14:43:41 +0200

    In shambles

---
+++ CsvParser/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+using System.Globalization;
+using CsvHelper;
+
+using FileStream fileA = File.Open("Data/Attachments-TechTest/Transactions-A.csv", FileMode.Open);
+using FileStream fileB = File.Open("Data/Attachments-TechTest/Transactions-B.csv", FileMode.Open);
+using var reader = new StreamReader(fileA);
+using var readerB = new StreamReader(fileB);
+using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+using var csvB = new CsvReader(readerB, CultureInfo.InvariantCulture);
+var recordsA = csv.GetRecords<Transaction>().ToList();
+var recordsB = csvB.GetRecords<Transaction>().ToList();
+Console.WriteLine($"Transaction A Count: {recordsA.Count}");
+Console.WriteLine($"Transaction B Count: {recordsB.Count}");
+
+var task1 = recordsA.Where(x => recordsB.All(y => y.TradeId != x.TradeId)).ToList();
+var dictionaryB = recordsB.ToDictionary(a => a.TradeId);
+foreach (Transaction transaction in recordsA)
+{
+    if (!dictionaryB.ContainsKey(transaction.TradeId))
+    {
+        Console.WriteLine(transaction);
+    }
+}
+
+Console.WriteLine($"Result1 count: {task1.Count}");
+foreach (Transaction transaction in task1)
+{
+    Console.WriteLine(transaction);
+}
+// var task2 = recordsB.Where(x => recordsA.All(y => y.TradeId != x.TradeId)).ToList();
+// Console.WriteLine($"Result2 count: {task2.Count}");
+// foreach (Transaction transaction2 in task2)
+// {
+//     Console.WriteLine(transaction2);
+// }
+
+// var task3 = recordsB.Where(b => recordsA.Any(a => a.TradeId == b.TradeId && a != b)).ToList();
+// // var task3 = recordsB.Intersect(recordsA, new TransactionTradeIdComparer()).ToList();
+// // var task3_a = recordsA.Where(x => task3.Select(y => y.TradeId).Any(y => y == x.TradeId));
+// Console.WriteLine($"Result3 count: {task3.Count}");
+// foreach (Transaction transaction3 in task3)
+// {
+//     Console.WriteLine(transaction3);
+// }
+
+var task4 = recordsB
+    .Join(recordsA, a => a.TradeId, b => b.TradeId, (a, b) => new { a, b })
+    .Where(join => join.a != join.b).ToList();
+Console.WriteLine($"Result4 count: {task4.Count}");
+foreach (var transaction4 in task4)
+{
+    Console.WriteLine(transaction4);
+}
+
+public record Transaction
+{
+    public string Ticker { get; set; }
+    public int TradeId { get; set; }
+    public string Counterparty { get; set; }
+    public decimal Quantity { get; set; }
+    public long CalcEstimate { get; set; }
+    public string TradeType { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Please create a .net console application that does reconciliation of the two data sources attached to this email (data source not included in the project).
+
+The result should be printed to the console in the following order:
+
+* All the transactions that are in the Transactions-A table but not in the Transactions-B table.
+* All the transactions that are in the Transactions-B table but not in the Transactions-A table.
+* All the values that are different for the same transaction that exists in both tables.
+
+Thank you
